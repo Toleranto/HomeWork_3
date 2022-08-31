1fb53cf6f1174bed923e76ba992dd1e054b80820
Eugenius <eugenius8447@gmail.com>
2022-08-31 21:33:36 +0300

    First method

---
+++ Exercise_19/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число
+// и проверяет, является ли оно палиндромом.
+Console.Clear();
+Console.WriteLine("Введите 5-ти значное число и вы узнаете являеться ли оно палиндромом: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string fiveNum = Convert.ToString(num);
+if (fiveNum[0] == fiveNum[4] && fiveNum[1] == fiveNum[3])
+{
+    Console.WriteLine($"Число {num} является палиндромом!!!");
+}
+else 
+{
+ Console.WriteLine($"Число {num} не является палиндромом!!!");
+}
